23ad30329617085be9d2fc2c196e1aad54d88da6
David Pearson <David@DavidPearson.biz>
2022-01-08 14:26:32 -0800

    commit before conversion to Sonatype / Maven Central distribution

--- lintrules/src/main/java/biz/davidpearson/android/customlintrules/CustomIssueRegistry.java
+++ lintrules/src/main/java/biz/davidpearson/android/customlintrules/CustomIssueRegistry.java
@@ -11,16 +11,18 @@ import java.util.List;
  * Created by David Pearson on 2017-05-09
  * <p>
  * Registry which provides a list of checks to be performed
+ * <p>
+ * https://googlesamples.github.io/android-custom-lint-rules/api-guide.html
  */
 public class CustomIssueRegistry extends IssueRegistry {
 
     @Override
     public List<Issue> getIssues() {
-        // return Arrays.asList(PrintStackTraceDetector.ISSUE_PRINT_STACK_TRACE);
         return Collections.singletonList(PrintStackTraceDetector.ISSUE_PRINT_STACK_TRACE);
     }
 
-    @Override public int getApi() {
+    @Override
+    public int getApi() {
         return com.android.tools.lint.detector.api.ApiKt.CURRENT_API;
     }
 }
